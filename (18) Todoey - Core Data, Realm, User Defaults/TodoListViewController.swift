f1201507d45552076fc050beecba5ac1e22e8d36
Amy Fang <amy.fang37@gmail.com>
2019-07-03 09:19:09 -0500

    Add new item functionality complete

--- (18) Todoey - Core Data, Realm, User Defaults/TodoListViewController.swift
+++ (18) Todoey - Core Data, Realm, User Defaults/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Print paper", "Drink a boba"]
+    var itemArray = ["Print paper", "Drink a boba"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,10 +45,39 @@ class TodoListViewController: UITableViewController {
         
         
         tableView.deselectRow(at: indexPath, animated: true) // creates the flash highlight when the row is selected
+
         
+    }
+    
+    //MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // what will happen once the user clicks the Add Item button
+            self.itemArray.append(textField.text ?? "")
+            
+            // refresh the tableView to reflect the newly added row
+            self.tableView.reloadData()
+            
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
         
         
     }
+    
 
 
 }
